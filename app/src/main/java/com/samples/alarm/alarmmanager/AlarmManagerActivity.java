3bc96484c346c10dc472904da62e70259589af95
java2019 <java_2019@mail.ru>
2017-06-24 13:45:36 +0300

    Initial commit

---
+++ app/src/main/java/com/samples/alarm/alarmmanager/AlarmManagerActivity.java
@@ -0,0 +1,50 @@
+package com.samples.alarm.alarmmanager;
+
+import android.app.Activity;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import java.util.Calendar;
+
+public class AlarmManagerActivity extends Activity
+        implements View.OnClickListener{
+
+    private PendingIntent pendingIntent;
+    private AlarmManager alarmManager;
+    private Intent intent;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alarm_manager);
+
+        alarmManager = (AlarmManager)getSystemService(ALARM_SERVICE);
+        intent = new Intent(AlarmManagerActivity.this, AlarmManager.class);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.bStart:
+                pendingIntent = PendingIntent.getService(getApplicationContext(), 0, intent, 0);
+                Calendar calendar = Calendar.getInstance();
+                calendar.setTimeInMillis(System.currentTimeMillis());
+                calendar.add(calendar.SECOND, 5);
+                alarmManager.setRepeating(
+                        AlarmManager.RTC_WAKEUP,
+                        calendar.getTimeInMillis(),
+                        5000,
+                        pendingIntent);
+                break;
+            case R.id.bCancel:
+                alarmManager.cancel(pendingIntent);
+                break;
+            case R.id.bStop:
+                this.stopService(intent);
+                break;
+        }
+    }
+}
---
+++ app/src/main/java/com/samples/alarm/alarmmanager/AlarmService.java
@@ -0,0 +1,45 @@
+package com.samples.alarm.alarmmanager;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.widget.Toast;
+
+/**
+ * Created by use on 24.06.17.
+ */
+public class AlarmService extends Service {
+
+    private int count;
+
+    @Override
+    public void onCreate() {
+        count = 0;
+        Toast.makeText(this, "Create service", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        Toast.makeText(this, "Bind service", Toast.LENGTH_SHORT).show();
+        return null;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(this, "Destroy service", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onStart(Intent intent, int startId) {
+        super.onStart(intent, startId);
+        count++;
+        Toast.makeText(this, "Notify drom alarm service #" + count, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public boolean onUnbind(Intent intent) {
+        Toast.makeText(this, "Unbind service", Toast.LENGTH_SHORT).show();
+        return super.onUnbind(intent);
+    }
+}
